7c24090e2c1aaec2e51ff11848f4b0ba31fda04d
Igor Podlesny <poige@users.noreply.github.com>
2017-01-12 13:50:50 +0700

    innodb_fast_shutdown, upgrade

--- mysql/mysql.sql
+++ mysql/mysql.sql
@@ -1,3 +1,9 @@
 -- faster shutdown: {
 set global innodb_max_dirty_pages_pct = 0;
 -- }
+
+-- before #upgrade, #checklist: {
+  /* 0: Use the slow shutdown technique before upgrading or downgrading between MySQL major releases, so
+  that all data files are fully prepared in case the upgrade process updates the file format. */
+  set global innodb_fast_shutdown = 0;
+-- }
